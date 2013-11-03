cf85ed486e51af564027c91d3720f0f03e0ae769
sander1 <sander@plexapp.com>
2013-11-03 02:01:41 +0100

    Move URL Service into the channel bundle.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,73 +1,82 @@
-import re
-
 NAME = 'Gamekings'
 BASE_URL = 'http://www.gamekings.tv'
 VIDEO = '%s/videos' % BASE_URL
 CATEGORY = '%s/category/%%s/page/%%d' % BASE_URL
-ART = 'art-default.jpg'
-ICON = 'icon-default.png'
+
+RE_NEXT_PAGE = Regex('Pagina (\\d+) van (\\d+)')
 
 ####################################################################################################
 def Start():
-  Plugin.AddPrefixHandler('/video/gamekings', MainMenu, NAME, ICON, ART)
-  Plugin.AddViewGroup('List', viewMode='List', mediaType='items')
-  Plugin.AddViewGroup('InfoList', viewMode='InfoList', mediaType='items')
 
-  ObjectContainer.art = R(ART)
   ObjectContainer.title1 = NAME
-  ObjectContainer.view_group = 'List'
-  DirectoryObject.thumb = R(ICON)
-
   HTTP.CacheTime = CACHE_1HOUR
-  HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:9.0.1) Gecko/20100101 Firefox/9.0.1'
+  HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:22.0) Gecko/20100101 Firefox/22.0'
 
 ####################################################################################################
+@handler('/video/gamekings', NAME)
 def MainMenu():
+
   oc = ObjectContainer()
 
   # The first list option is different from the rest (it's missing the "class" attribute)
-  oc.add(DirectoryObject(key=Callback(Playlist, id='videos', category_title='Alle video\'s'), title='Alle video\'s'))
+  oc.add(DirectoryObject(
+    key = Callback(Playlist, id='videos', category_title='Alle video\'s'),
+    title = 'Alle video\'s'
+  ))
 
   # All top level menu items
   list = HTML.ElementFromURL(VIDEO).xpath('//select[@id="cat"]/option[@class="level-0"]')
 
   for item in list:
-    id    = item.get('value')
+    id = item.get('value')
     title = item.xpath('./text()')[0].strip()
 
     # Check to see if an item has 'sub'-items (siblings with class="level-1")
-    if len(item.xpath('./following-sibling::*')) == 0 or item.xpath('./following-sibling::*')[0].get('class') == 'level-0':
-      oc.add(DirectoryObject(key=Callback(Playlist, id=id, category_title=title), title=title))
+    if len(item.xpath('./following-sibling::*')) == 0 or item.xpath('./following-sibling::*/@class')[0] == 'level-0':
+      oc.add(DirectoryObject(
+        key = Callback(Playlist, id=id, category_title=title),
+        title = title
+      ))
     else:
       siblings = item.xpath('./following-sibling::*')
       sub = []
 
       for s in siblings:
-        sib_id    = s.get('value')
+        sib_id = s.get('value')
         sib_title = s.xpath('./text()')[0].strip()
         sib_level = s.get('class')
 
         if sib_level == 'level-0':
-          break;
+          break
         else:
           sub.append([sib_id, sib_title])
 
-      oc.add(DirectoryObject(key=Callback(Subcategory, sub=sub, category_title=title), title=title))
+      oc.add(DirectoryObject(
+        key = Callback(Subcategory, sub=sub, category_title=title),
+        title = title
+      ))
 
   return oc
 
 ####################################################################################################
+@route('/video/gamekings/category')
 def Subcategory(sub, category_title):
+
   oc = ObjectContainer(title2=category_title)
 
   for (id, title) in sub:
-    oc.add(DirectoryObject(key=Callback(Playlist, id=id, category_title=title), title=title))
+    oc.add(DirectoryObject(
+      key = Callback(Playlist, id=id, category_title=title),
+      title = title
+    ))
 
   return oc
 
 ####################################################################################################
+@route('/video/gamekings/playlist', page=int)
 def Playlist(id, category_title, page=1):
-  oc = ObjectContainer(view_group='InfoList', title2=category_title)
+
+  oc = ObjectContainer(title2=category_title)
   content = HTML.ElementFromURL(CATEGORY % (id, page))
 
   for video in content.xpath('//section[@id="archiefoverzicht"]/article/a'):
@@ -96,30 +105,37 @@ def Playlist(id, category_title, page=1):
   # Pagination
   try:
     # Find out how many pages there are in total
-    total_pages = re.search('Pagina (\\d+) van (\\d+)', content.xpath('//ul[@id="paginate"]')[0].text_content()).group(2)
+    total_pages = RE_NEXT_PAGE.search(content.xpath('//ul[@id="paginate"]')[0].text_content()).group(2)
 
     # Add a 'More...' element if there is more than one page with videos
     if page < int(total_pages):
-      oc.add(DirectoryObject(key=Callback(Playlist, id=id, category_title=category_title, page=page+1), title='Meer...'))
+      oc.add(NextPageObject(
+        key = Callback(Playlist, id=id, category_title=category_title, page=page+1),
+        title = 'Meer...'
+      ))
   except:
     pass
 
   return oc
 
 ####################################################################################################
+@route('/video/gamekings/getthumb')
 def GetThumb(url):
+
   if url:
     try:
-      data = HTTP.Request(url.replace(' ', '%20'), cacheTime=CACHE_1MONTH).content
+      data = HTTP.Request(url.replace(' ', '%20'), cacheTime=CACHE_1MONTH, sleep=0.5).content
       return DataObject(data, 'image/jpeg')
     except:
       pass
 
-  return Redirect(R(ICON))
+  return Redirect('http://resources-cdn.plexapp.com/image/source/com.plexapp.plugins.gamekings.jpg')
 
 ####################################################################################################
+@route('/video/gamekings/getepisodethumb')
 def GetEpisodeThumb(url):
-  thumb = HTML.ElementFromURL(url, cacheTime=CACHE_1MONTH).xpath('//img[@id="videocover"]')
+
+  thumb = HTML.ElementFromURL(url, cacheTime=CACHE_1MONTH, sleep=0.5).xpath('//img[@id="videocover"]')
 
   if thumb:
     thumb = thumb[0].get('src')
